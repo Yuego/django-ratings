f6bef68a4a5941d24beaf94e61daf51d89b0de9e
Yuego [Lenovo] <root@proscript.ru>
2014-02-04 22:28:41 +0400

    Django TestRunner support

--- djangoratings/runtests.py
+++ djangoratings/runtests.py
@@ -7,15 +7,35 @@ from django.conf import settings
 
 if not settings.configured:
     settings.configure(
-        DATABASE_ENGINE='sqlite3',
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': 'haptachi',
+                'USER': '',
+                'PASSWORD': '',
+                'HOST': '',
+                'PORT': '',
+            },
+        },
         INSTALLED_APPS=[
             'django.contrib.auth',
             'django.contrib.contenttypes',
             'djangoratings',
-        ]
+        ],
+        SECRET_KEY='test',
     )
 
-from django.test.simple import run_tests
+try:
+    from django.test.simple import DjangoTestSuiteRunner as TestRunner
+except ImportError:
+    from django.test.simple import run_tests
+
+    class TestRunner(object):
+        def __init__(self, **kwargs):
+            self.kwargs = kwargs
+
+        def run_tests(self, apps):
+            return run_tests(apps, **self.kwargs)
 
 
 def runtests(*test_args):
@@ -23,7 +43,8 @@ def runtests(*test_args):
         test_args = ['djangoratings']
     parent = dirname(abspath(__file__))
     sys.path.insert(0, parent)
-    failures = run_tests(test_args, verbosity=1, interactive=True)
+    tr = TestRunner(verbosity=1, interactive=True)
+    failures = tr.run_tests(test_args)
     sys.exit(failures)
 
 
